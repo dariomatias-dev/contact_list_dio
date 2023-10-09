24c8127d71309f32293a8f49987c679593c0ae53
dario <matiasdario75@gmail.com>
2023-10-09 09:23:16 -0300

    feat(ContactModel): updating the contact model
    - Updated contact model with new fields.

--- lib/src/models/contact_model.dart
+++ lib/src/models/contact_model.dart
@@ -1,9 +1,47 @@
 class ContactModel {
   ContactModel({
     required this.name,
+    required this.nickname,
     required this.number,
+    required this.email,
+    required this.address,
+    required this.grades,
+    required this.createdAt,
+    required this.updatedAt,
   });
 
   final String name;
+  final String nickname;
   final String number;
+  final String email;
+  final String address;
+  final String grades;
+  final DateTime createdAt;
+  final DateTime updatedAt;
+
+  factory ContactModel.toMap(Map<String, dynamic> map) {
+    return ContactModel(
+      name: map['name'],
+      nickname: map['nickname'],
+      number: map['number'],
+      email: map['email'],
+      address: map['address'],
+      grades: map['grades'],
+      createdAt: map['createdAt'],
+      updatedAt: map['updatedAt'],
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'name': name,
+      'nickname': nickname,
+      'number': number,
+      'email': email,
+      'address': address,
+      'grades': grades,
+      'createdAt': createdAt.millisecondsSinceEpoch,
+      'updatedAt': updatedAt.millisecondsSinceEpoch,
+    };
+  }
 }
--- lib/src/screens/home_screen/home_screen.dart
+++ lib/src/screens/home_screen/home_screen.dart
@@ -11,23 +11,56 @@ import 'package:contact_list/src/screens/search_screen/search_screen.dart';
 final List<ContactModel> contacts = <ContactModel>[
   ContactModel(
     name: 'Dário',
+    nickname: '',
     number: '+55 (83) 98640-4371',
+    email: 'matiasdario75@gmail.com',
+    address: 'Praça Central, 789',
+    grades: '',
+    createdAt: DateTime(2023, 6, 10),
+    updatedAt: DateTime(2023, 8, 30),
   ),
   ContactModel(
     name: 'Silva',
+    nickname: 'Silvana',
     number: '+55 (83) 97832-0725',
+    email: 'silva@gmail.com',
+    address: 'Estrada Secundária, 321',
+    grades:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    createdAt: DateTime(2023, 7, 5),
+    updatedAt: DateTime(2023, 9, 15),
   ),
   ContactModel(
     name: 'Luiz',
+    nickname: 'Lu',
     number: '+55 (83) 96293-0265',
+    email: 'luiz@gmail.com',
+    address: 'Avenida Norte, 654',
+    grades: '',
+    createdAt: DateTime(2023, 8, 1),
+    updatedAt: DateTime(2023, 10, 5),
   ),
   ContactModel(
     name: 'Mariana',
+    nickname: 'Mari',
     number: '+55 (83) 97143-7193',
+    email: 'mariana@gmail.com',
+    address: 'Rua das Flores, 123',
+    grades:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    createdAt: DateTime(2023, 4, 15),
+    updatedAt: DateTime(2023, 10, 1),
   ),
   ContactModel(
     name: 'Clara',
+    nickname:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     number: '+55 (83) 99165-9105',
+    email: 'clara@gmail.com',
+    address: 'Avenida Principal, 456',
+    grades: '',
+    createdAt: DateTime(2023, 5, 20),
+    updatedAt: DateTime(2023, 9, 25),
   ),
 ];
 
