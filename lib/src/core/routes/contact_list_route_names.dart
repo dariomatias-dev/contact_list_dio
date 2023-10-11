3a31323913f321724eac582f9d3e0b0ad0d00865
dario <matiasdario75@gmail.com>
2023-10-11 08:42:01 -0300

    feat: contact form screen widget created and route added to ContactListRouteNames
    - Contact form screen widget created.
    - Navigation route for contact form screen added in ContactListRouteNames.

--- lib/src/core/routes/contact_list_route_names.dart
+++ lib/src/core/routes/contact_list_route_names.dart
@@ -2,4 +2,5 @@ class ContactListRouteNames {
   static const String home = '/';
   static const String search = '/search';
   static const String contact = '/contact';
+  static const String contactForm = '/contact_form';
 }
